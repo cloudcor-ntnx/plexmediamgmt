8154357892a5db02781d23d1940aabb0d2f3a5d6
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2020-09-13 16:34:15 -0700

    Create PlexMovieFolder.ps1

---
+++ PlexMovieFolder.ps1
@@ -0,0 +1,9 @@
+	   <# 
+	   
+        .SYNOPSIS
+            Prompts for name of new movie folder
+    #>
+
+$moviefolder = Read-host -prompt 'Enter the Name of the Container'
+New-Item -Path "Z:\MOVIES" -Name "$folderName" -ItemType "directory"
+            write-host "Creating Folder..."
