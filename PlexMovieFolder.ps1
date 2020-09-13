581f2df36fcf5026ca043396755fbd4b0222ce74
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2020-09-13 16:53:14 -0700

    Update PlexMovieFolder.ps1

--- PlexMovieFolder.ps1
+++ PlexMovieFolder.ps1
@@ -5,5 +5,5 @@
     #>
 
 $moviefolder = Read-host -prompt 'Enter the Name of the Movie'
-New-Item -Path "Z:\MOVIES" -Name "$moviefolder" -ItemType "directory"
+New-Item -Path "\\10.0.0.11\USER-DATA-SHARES\TV-SHOWS\" -Name "$moviefolder" -ItemType "directory"
             write-host "Creating Folder..."
