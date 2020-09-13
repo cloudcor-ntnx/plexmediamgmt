f55984bf8917ce35942ccf2595b412f2cfc29458
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2020-09-13 16:50:44 -0700

    Update PlexTVSHOWFolder.ps1

--- PlexTVSHOWFolder.ps1
+++ PlexTVSHOWFolder.ps1
@@ -4,6 +4,6 @@
             Prompts for name of new TV Show folder
     #>
 
-$TVfolder = Read-host -prompt 'Enter the Name of the Container'
+$TVfolder = Read-host -prompt 'Enter the Name of the TV Show'
 New-Item -Path "Z:\TV-SHOWS" -Name "$folderName" -ItemType "directory"
             write-host "Creating Folder..."
