b73ec93f4d3f260a53e596f0ddbd5d503a71d177
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2020-09-13 16:50:14 -0700

    Update PlexMovieFolder.ps1

--- PlexMovieFolder.ps1
+++ PlexMovieFolder.ps1
@@ -4,6 +4,6 @@
             Prompts for name of new movie folder
     #>
 
-$moviefolder = Read-host -prompt 'Enter the Name of the Container'
+$moviefolder = Read-host -prompt 'Enter the Name of the Movie'
 New-Item -Path "Z:\MOVIES" -Name "$folderName" -ItemType "directory"
             write-host "Creating Folder..."
