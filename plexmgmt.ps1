eb1a4262e29218c6908523b539e5682f2f102b97
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2020-09-16 19:55:47 -0700

    Update plexmgmt.ps1

--- plexmgmt.ps1
+++ plexmgmt.ps1
@@ -4,15 +4,15 @@ function mainMenu {
         Clear-Host
         Write-Host "`n`t`t Plex Media Mgmt`n"
         Write-Host -ForegroundColor DarkCyan -NoNewline "`n["; Write-Host -NoNewline "1"; Write-Host -ForegroundColor DarkCyan -NoNewline "]"; `
-            Write-Host -ForegroundColor DarkCyan " Create MKV Folder"
+            Write-Host -ForegroundColor DarkCyan " New Movie?"
         Write-Host -ForegroundColor DarkCyan -NoNewline "`n["; Write-Host -NoNewline "2"; Write-Host -ForegroundColor DarkCyan -NoNewline "]"; `
-            Write-Host -ForegroundColor DarkCyan " Create Plex Folder"
+            Write-Host -ForegroundColor DarkCyan " New TV-Show?"
         $mainMenu = Read-Host "`nSelection (leave blank to quit)"
-        # Create a folder for makemkv 
+        # Movie Folder  
         if($mainMenu -eq 1){
             subMenu1
         }
-        # Create folder on the Plex Media directories 
+        # TV Show Folder  
         if($mainMenu -eq 2){
             subMenu2
         }
@@ -21,53 +21,63 @@ function mainMenu {
 
 function subMenu1 {
     $subMenu1 = 'X'
-    if($subMenu1 -ne ''){
-        Clear-Host
-        Write-Host "`n`t`t Create new makemkv folder`n"
-        Write-Host -ForegroundColor Cyan "Enter the folder name"
-            $folderName = read-host "MKV Folder"
-            New-Item -Path "C:\Users\danny\Videos\MakeMKV" -Name "$folderName" -ItemType "directory"
-            write-host "Creating Folder..."
-
-        }
-
-}
-function subMenu2 {
-    $subMenu2 = 'X'
-    while($subMenu2 -ne ''){
+    while($subMenu1 -ne ''){
         Clear-Host
         Write-Host "`n`t`t Plex Media Mgmt`n"
         Write-Host -ForegroundColor DarkCyan -NoNewline "`n["; Write-Host -NoNewline "1"; Write-Host -ForegroundColor DarkCyan -NoNewline "]"; `
-            Write-Host -ForegroundColor DarkCyan " Movie?"
+            Write-Host -ForegroundColor DarkCyan " For MakeMKV?"
         Write-Host -ForegroundColor DarkCyan -NoNewline "`n["; Write-Host -NoNewline "2"; Write-Host -ForegroundColor DarkCyan -NoNewline "]"; `
-            Write-Host -ForegroundColor DarkCyan " TV Show?"
-	Write-Host -ForegroundColor DarkCyan -NoNewline "`n["; Write-Host -NoNewline "3"; Write-Host -ForegroundColor DarkCyan -NoNewline "]"; `
-            Write-Host -ForegroundColor DarkCyan " Exit?"
-        $subMenu2 = Read-Host "`nSelection (leave blank to quit)"
+            Write-Host -ForegroundColor DarkCyan " For Plex Library?"
+        $subMenu1 = Read-Host "`nSelection (leave blank to quit)"
         # Movie 
-        if($subMenu2 -eq 1){
-            Write-Host "New Movie Folder"
-            $movie = invoke-webrequest https://raw.githubusercontent.com/cloudcor-ntnx/plexmediamgmt/master/PlexMovieFolder.ps1
+        if($subMenu1 -eq 1){
+            Write-Host "New MKV Folder"
+            $movie = invoke-webrequest https://raw.githubusercontent.com/cloudcor-ntnx/plexmediamgmt/master/MakeMKVMovie.ps1
             invoke-expression $($movie.content)
 			Write-Host -ForegroundColor DarkCyan "`nScript execution complete."
             Write-Host "`nPress any key to return to the previous menu"
             [void][System.Console]::ReadKey($true)
         }
         # TV Show
+        if($subMenu1 -eq 2){
+            Write-Host "New Plex Library Folder (default is the root folder)"
+            $tvshow = invoke-webrequest https://raw.githubusercontent.com/cloudcor-ntnx/plexmediamgmt/master/PlexMovieFolder.ps1 
+            invoke-expression $($tvshow.content)
+			Write-Host -ForegroundColor DarkCyan "`nScript execution complete."
+            Write-Host "`nPress any key to return to the previous menu"
+            [void][System.Console]::ReadKey($true)
+        }
+        }
+    }
+function subMenu2 {
+    $subMenu2 = 'X'
+    while($subMenu2 -ne ''){
+        Clear-Host
+        Write-Host "`n`t`t Plex Media Mgmt`n"
+        Write-Host -ForegroundColor DarkCyan -NoNewline "`n["; Write-Host -NoNewline "1"; Write-Host -ForegroundColor DarkCyan -NoNewline "]"; `
+            Write-Host -ForegroundColor DarkCyan " For MKV?"
+        Write-Host -ForegroundColor DarkCyan -NoNewline "`n["; Write-Host -NoNewline "2"; Write-Host -ForegroundColor DarkCyan -NoNewline "]"; `
+            Write-Host -ForegroundColor DarkCyan " For Plex Library?"
+        $subMenu2 = Read-Host "`nSelection (leave blank to quit)"
+        # MKV TV Show  
+        if($subMenu2 -eq 1){
+            Write-Host "New MKV TV Show folder"
+            $movie = invoke-webrequest https://raw.githubusercontent.com/cloudcor-ntnx/plexmediamgmt/master/MakeMKVTV.ps1
+            invoke-expression $($movie.content)
+			Write-Host -ForegroundColor DarkCyan "`nScript execution complete."
+            Write-Host "`nPress any key to return to the previous menu"
+            [void][System.Console]::ReadKey($true)
+        }
+        # Plex Library TV Show Folder 
         if($subMenu2 -eq 2){
-            Write-Host "New TV Show Folder"
+            Write-Host "New Plex TV Show Folder"
             $tvshow = invoke-webrequest https://raw.githubusercontent.com/cloudcor-ntnx/plexmediamgmt/master/PlexTVSHOWFolder.ps1
             invoke-expression $($tvshow.content)
 			Write-Host -ForegroundColor DarkCyan "`nScript execution complete."
             Write-Host "`nPress any key to return to the previous menu"
             [void][System.Console]::ReadKey($true)
         }
-        # Exit 
-        if($subMenu2 -eq 3){
-         exit
-        }
         }
     }
-}
 
 mainMenu
