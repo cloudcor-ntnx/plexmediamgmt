3b6bb5d1393660dc9c2e7646e9ce2013895e1afc
cloudcor-ntnx <55808532+cloudcor-ntnx@users.noreply.github.com>
2020-09-27 15:48:28 -0700

    Update createfoldersMenu.ps1

--- create_folders/createfoldersMenu.ps1
+++ create_folders/createfoldersMenu.ps1
@@ -29,19 +29,19 @@ function subMenu1 {
         Write-Host -ForegroundColor DarkCyan -NoNewline "`n["; Write-Host -NoNewline "2"; Write-Host -ForegroundColor DarkCyan -NoNewline "]"; `
             Write-Host -ForegroundColor DarkCyan " For Plex Library?"
         $subMenu1 = Read-Host "`nSelection (leave blank to quit)"
-        # Movie 
+        # MKV Movie  
         if($subMenu1 -eq 1){
             Write-Host "New MKV Folder"
-            $movie = invoke-webrequest https://raw.githubusercontent.com/cloudcor-ntnx/plexmediamgmt/master/MakeMKVMovie.ps1
+            $movie = invoke-webrequest https://raw.githubusercontent.com/cloudcor-ntnx/plexmediamgmt/master/create_folders/MakeMKVMovie.ps1
             invoke-expression $($movie.content)
 			Write-Host -ForegroundColor DarkCyan "`nScript execution complete."
             Write-Host "`nPress any key to return to the previous menu"
             [void][System.Console]::ReadKey($true)
         }
-        # TV Show
+        # Plex Movie
         if($subMenu1 -eq 2){
             Write-Host "New Plex Library Folder (default is the root folder)"
-            $tvshow = invoke-webrequest https://raw.githubusercontent.com/cloudcor-ntnx/plexmediamgmt/master/PlexMovieFolder.ps1 
+            $tvshow = invoke-webrequest https://raw.githubusercontent.com/cloudcor-ntnx/plexmediamgmt/master/create_folders/PlexMovieFolder.ps1 
             invoke-expression $($tvshow.content)
 			Write-Host -ForegroundColor DarkCyan "`nScript execution complete."
             Write-Host "`nPress any key to return to the previous menu"
@@ -62,7 +62,7 @@ function subMenu2 {
         # MKV TV Show  
         if($subMenu2 -eq 1){
             Write-Host "New MKV TV Show folder"
-            $movie = invoke-webrequest https://raw.githubusercontent.com/cloudcor-ntnx/plexmediamgmt/master/MakeMKVTV.ps1
+            $movie = invoke-webrequest https://raw.githubusercontent.com/cloudcor-ntnx/plexmediamgmt/master/create_folders/MakeMKVTV.ps1
             invoke-expression $($movie.content)
 			Write-Host -ForegroundColor DarkCyan "`nScript execution complete."
             Write-Host "`nPress any key to return to the previous menu"
@@ -71,7 +71,7 @@ function subMenu2 {
         # Plex Library TV Show Folder 
         if($subMenu2 -eq 2){
             Write-Host "New Plex TV Show Folder"
-            $tvshow = invoke-webrequest https://raw.githubusercontent.com/cloudcor-ntnx/plexmediamgmt/master/PlexTVSHOWFolder.ps1
+            $tvshow = invoke-webrequest https://raw.githubusercontent.com/cloudcor-ntnx/plexmediamgmt/master/create_folders/PlexTVSHOWFolder.ps1
             invoke-expression $($tvshow.content)
 			Write-Host -ForegroundColor DarkCyan "`nScript execution complete."
             Write-Host "`nPress any key to return to the previous menu"
